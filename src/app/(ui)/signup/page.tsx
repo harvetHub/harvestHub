1e57e30c4a2758f672270c1af0316a17767a67c7
yusri <yusripawaki2@gmail.com>
2025-03-06 14:49:31 +0800

    Update layout metadata and implement MainLayout component; add user SVG and enhance Home page with links

--- src/app/(ui)/signup/page.tsx
+++ src/app/(ui)/signup/page.tsx
@@ -2,18 +2,30 @@
 
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
-import Link from "next/link";
 import { Input } from "@/components/ui/input";
+import Link from "next/link";
 
-export default function Home() {
+export default function Signup() {
+  const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [usernameError, setUsernameError] = useState("");
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState("");
 
-  const handleLogin = (e: React.FormEvent) => {
+  const handleSignup = (e: React.FormEvent) => {
     e.preventDefault();
     let valid = true;
+
+    if (!username) {
+      setUsernameError("Username is required");
+      valid = false;
+    } else {
+      setUsernameError("");
+    }
+
     if (!email) {
       setEmailError("Email is required");
       valid = false;
@@ -28,8 +40,15 @@ export default function Home() {
       setPasswordError("");
     }
 
+    if (password !== confirmPassword) {
+      setConfirmPasswordError("Passwords do not match");
+      valid = false;
+    } else {
+      setConfirmPasswordError("");
+    }
+
     if (valid) {
-      // Handle login logic here
+      // Handle signup logic here
     }
   };
 
@@ -44,15 +63,51 @@ export default function Home() {
         <div className="flex items-center justify-center px-4 py-10 bg-white sm:px-6 lg:px-8 sm:py-16 lg:py-24">
           <div className="xl:w-full xl:max-w-sm 2xl:max-w-md xl:mx-auto">
             <h2 className="text-3xl font-bold text-center leading-tight text-black sm:text-4xl">
-              Sign in to HarvestHub
+              Sign up for HarvestHub
             </h2>
 
             <h3 className="my-4 font-semibold opacity-80 text-center">
               Integrated Online Agricultural Marketplace
             </h3>
 
-            <form onSubmit={handleLogin} className="mt-8">
+            <form onSubmit={handleSignup} className="mt-8">
               <div className="space-y-5">
+                <div>
+                  <label
+                    htmlFor="username"
+                    className="text-base font-medium text-gray-900"
+                  >
+                    Username
+                  </label>
+                  <div className="mt-2.5 relative text-gray-400 focus-within:text-gray-600">
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 h-9 pointer-events-none">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        className="w-5 h-5"
+                      >
+                        <path
+                          fill="currentColor"
+                          d="M9.993 10.573a4.5 4.5 0 1 0 0-9a4.5 4.5 0 0 0 0 9ZM10 0a6 6 0 0 1 3.04 11.174c3.688 1.11 6.458 4.218 6.955 8.078c.047.367-.226.7-.61.745c-.383.045-.733-.215-.78-.582c-.54-4.19-4.169-7.345-8.57-7.345c-4.425 0-8.101 3.161-8.64 7.345c-.047.367-.397.627-.78.582c-.384-.045-.657-.378-.61-.745c.496-3.844 3.281-6.948 6.975-8.068A6 6 0 0 1 10 0Z"
+                        />
+                      </svg>
+                    </div>
+                    <Input
+                      type="text"
+                      id="username"
+                      placeholder="Enter your username"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                      className="block w-full py-4 pl-10 pr-4 text-black placeholder-gray-500 transition-all duration-200 border border-gray-200 rounded-md bg-gray-50 focus:outline-none focus:border-blue-600 focus:bg-white caret-blue-600"
+                    />
+                    {usernameError && (
+                      <p className="text-destructive text-sm mt-2">
+                        {usernameError}
+                      </p>
+                    )}
+                  </div>
+                </div>
+
                 <div>
                   <label
                     htmlFor="email"
@@ -80,7 +135,7 @@ export default function Home() {
                     <Input
                       type="email"
                       id="email"
-                      placeholder="Enter email to get started"
+                      placeholder="Enter your email"
                       value={email}
                       onChange={(e) => setEmail(e.target.value)}
                       className="block w-full py-4 pl-10 pr-4 text-black placeholder-gray-500 transition-all duration-200 border border-gray-200 rounded-md bg-gray-50 focus:outline-none focus:border-blue-600 focus:bg-white caret-blue-600"
@@ -94,22 +149,12 @@ export default function Home() {
                 </div>
 
                 <div>
-                  <div className="flex items-center justify-between">
-                    <label
-                      htmlFor="password"
-                      className="text-base font-medium text-gray-900"
-                    >
-                      Password
-                    </label>
-
-                    <a
-                      href="#"
-                      title=""
-                      className="text-sm font-medium text-blue-600 transition-all duration-200 hover:text-blue-700 focus:text-blue-700 hover:underline"
-                    >
-                      Forgot password?
-                    </a>
-                  </div>
+                  <label
+                    htmlFor="password"
+                    className="text-base font-medium text-gray-900"
+                  >
+                    Password
+                  </label>
                   <div className="mt-2.5 relative text-gray-400 focus-within:text-gray-600">
                     <div className="absolute inset-y-0 left-0 flex items-center h-9 pl-3 pointer-events-none">
                       <svg
@@ -143,14 +188,53 @@ export default function Home() {
                   </div>
                 </div>
 
+                <div>
+                  <label
+                    htmlFor="confirmPassword"
+                    className="text-base font-medium text-gray-900"
+                  >
+                    Confirm Password
+                  </label>
+                  <div className="mt-2.5 relative text-gray-400 focus-within:text-gray-600">
+                    <div className="absolute inset-y-0 left-0 flex items-center h-9 pl-3 pointer-events-none">
+                      <svg
+                        className="w-5 h-5"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4"
+                        />
+                      </svg>
+                    </div>
+                    <Input
+                      type="password"
+                      id="confirmPassword"
+                      placeholder="Confirm your password"
+                      value={confirmPassword}
+                      onChange={(e) => setConfirmPassword(e.target.value)}
+                      className="block w-full py-4 pl-10 pr-4 text-black placeholder-gray-500 transition-all duration-200 border border-gray-200 rounded-md bg-gray-50 focus:outline-none focus:border-blue-600 focus:bg-white caret-blue-600"
+                    />
+                    {confirmPasswordError && (
+                      <p className="text-destructive text-sm mt-2">
+                        {confirmPasswordError}
+                      </p>
+                    )}
+                  </div>
+                </div>
                 <p className="mt-2 text-right text-gray-600">
-                  Don’t have an account?{" "}
+                  Already have an account?{" "}
                   <Link
-                    href="signup"
+                    href="/"
                     title=""
                     className="font-medium text-blue-600 transition-all duration-200 hover:text-blue-700 focus:text-blue-700 hover:underline"
                   >
-                    Signup
+                    Login
                   </Link>
                 </p>
 
@@ -159,7 +243,7 @@ export default function Home() {
                     type="submit"
                     className="inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-white transition-all duration-200 cursor-pointer border-transparent rounded-md bg-gradient-to-r from-green-400 to-blue-500 focus:outline-none hover:opacity-80 focus:opacity-80"
                   >
-                    Log in
+                    Sign up
                   </Button>
                 </div>
               </div>
