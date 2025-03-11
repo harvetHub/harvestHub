7b39f1469343617a2227ac9da72b0e2c83784c60
yusri <yusripawaki2@gmail.com>
2025-03-11 13:38:05 +0800

    Refactor dashboard layout to use MainLayout component; add Header and Footer to MainLayout

--- src/app/(ui)/dashboard/page.tsx
+++ src/app/(ui)/dashboard/page.tsx
@@ -1,15 +1,13 @@
 "use client";
 
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import { MainLayout } from "@/layout/MainLayout";
 import FeaturedProductCarousel from "@/components/dashboard/Featured";
 import CategoriesSection from "@/components/dashboard/Categories";
 import RecommendedItemsSection from "@/components/dashboard/Recommended";
 
 export default function Dashboard() {
   return (
-    <div>
-      <Header />
+    <MainLayout>
       <div className="container mx-auto p-4">
         <h1 className="text-3xl font-bold text-center mb-8">HarvestHub Shop</h1>
 
@@ -22,7 +20,6 @@ export default function Dashboard() {
         <h2 className="text-2xl font-bold mt-8 mb-4">Recommended Items</h2>
         <RecommendedItemsSection />
       </div>
-      <Footer />
-    </div>
+    </MainLayout>
   );
 }
