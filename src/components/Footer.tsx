d84d42ef8a840f6ca2530f240d7edf72390f1b05
yusri <yusripawaki2@gmail.com>
2025-05-06 08:33:19 +0800

    Update Footer component to use 'myContainer' class for consistent styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 const Footer: FC = () => {
   return (
     <footer className="bg-gray-800 text-white p-4">
-      <div className="container mx-auto text-center">
+      <div className="myContainer mx-auto text-center">
         <p>
           &copy; {new Date().getFullYear()} HarvestHub. All rights reserved.
         </p>
